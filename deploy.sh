40f50e3a3f3be2f9bbd52e75b3232c612929dc36
Mohmamad Arab <boynux@gmail.com>
2022-02-24 22:46:08 +0000

    Fix Docker image tagging

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,7 @@ IMAGE_TAG=${TRAVIS_TAG:-latest}
 
 docker --version
 docker build -t $IMAGE_NAME:$IMAGE_TAG .
+docker tag $IMAGE_NAME:$IMAGE_TAG $IMAGE_NAME:latest
 echo $DOCKER_API_KEY | docker login -u boynux --password-stdin
 docker push $IMAGE_NAME:$IMAGE_TAG
+docker push $IMAGE_NAME:latest
